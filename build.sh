407236fbb6701c098140dca9f616a18441ec83c4
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-04-26 01:34:12 -0300

    update script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ dockerid=$2
 version=$3
 machine=$(uname -m)
 
+mkdir -p files/root/etc/nginx/conf.d/
 sed 's/__COLOR__/'$color'/g' default.conf > files/root/etc/nginx/conf.d/default.conf
 docker build . -t ngnix-alpine-curl-hostname-$color-$machine
 rm files/root/etc/nginx/conf.d/default.conf
